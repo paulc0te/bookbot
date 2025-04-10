ae38435c833a5a94c0a62eb5dfae92fbc32bb025
paulc0te <alexcorpit@gmail.com>
2025-04-10 12:16:00 +0200

    Print a Report. First version

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 from stats import *
 
-file_path = "books/frankenstein.txt"
-
 def main():
+
+    file_path = "books/frankenstein.txt"
+
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {file_path}...")
+    print("----------- Word Count ----------")
     print(get_book_text(file_path))
+    print("--------- Character Count -------")
     dict_characters = get_characters(file_path)
-    for key, value in dict_characters.items():
-        print(f"'{key}': {value}")
+    # for key, value in dict_characters.items():
+    #     print(f"'{key}': {value}")
+    sorted_list_characters = get_sorted_list_characters(dict_characters)
+    for dict_characters in sorted_list_characters:
+        if dict_characters["character"].isalpha():
+            # с этим задание не принимают, но программа работает
+            # print(f"{dict_characters["character"]}: {dict_characters["count_char"]}")
+            # с этим задание принимают
+            print(f'{dict_characters["character"]}: {dict_characters["count_char"]}')
+
 main()
--- stats.py
+++ stats.py
@@ -8,9 +8,6 @@ def get_book_text(file_path):
     return f"{num_words} words found in the document"
 
 def get_characters(file_path):
-    """
-    Get number of characters
-    """
     with open(file_path) as f:
         file_contents = f.read().replace('\n', '')  # del \n
     dict_characters = {}
@@ -20,3 +17,10 @@ def get_characters(file_path):
         else:
             dict_characters[character.lower()] += 1
     return dict_characters
+
+def get_sorted_list_characters(dict_characters):
+    sorted_list_characters = []
+    for key, value in dict_characters.items():
+        sorted_list_characters.append({"character": key, "count_char": value})
+    result = sorted(sorted_list_characters, reverse=True, key=lambda dict_characters: dict_characters["count_char"])
+    return result
