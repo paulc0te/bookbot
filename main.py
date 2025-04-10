88a55ce9316d18756c4456928b14e1cbf1f85d11
paulc0te <alexcorpit@gmail.com>
2025-04-10 10:45:01 +0200

    Count Characters

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-from stats import get_book_text
+from stats import *
+
+file_path = "books/frankenstein.txt"
 
 def main():
-    print(get_book_text("books/frankenstein.txt"))
-
+    print(get_book_text(file_path))
+    dict_characters = get_characters(file_path)
+    for key, value in dict_characters.items():
+        print(f"'{key}': {value}")
 main()
--- stats.py
+++ stats.py
@@ -1,5 +1,22 @@
-def get_book_text(path_to_file):
-    with open(path_to_file) as f:
+def get_book_text(file_path):
+    """
+    Get number of words
+    """
+    with open(file_path) as f:
         file_contents = f.read()
     num_words = len(file_contents.split())
     return f"{num_words} words found in the document"
+
+def get_characters(file_path):
+    """
+    Get number of characters
+    """
+    with open(file_path) as f:
+        file_contents = f.read().replace('\n', '')  # del \n
+    dict_characters = {}
+    for character in file_contents:
+        if character.lower() not in dict_characters:
+            dict_characters[character.lower()] = 1
+        else:
+            dict_characters[character.lower()] += 1
+    return dict_characters
