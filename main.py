e0cb837cc1b326e0588255a9245c416544d6f955
paulc0te <alexcorpit@gmail.com>
2025-04-21 16:42:14 +0200

    The project was successfully done.

--- main.py
+++ main.py
@@ -3,11 +3,13 @@ from stats import (
     get_characters,
     get_sorted_list_characters
 )
+import sys
 
 def main():
-
-    file_path = "books/frankenstein.txt"
-
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    file_path = sys.argv[1]
     print("============ BOOKBOT ============")
     print(f"Analyzing book found at {file_path}...")
     print("----------- Word Count ----------")
